be0ff2ae85fd83ac543c01c6f42252064b5437d0
willbowden <willbowden03@yahoo.co.uk>
2025-03-31 22:24:27 +0800

    Experimenting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use std::sync::{Arc, Mutex};
+
+use cpal::{StreamConfig, SupportedOutputConfigs};
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+
 fn main() {
-    println!("Hello, world!");
+    let host = cpal::default_host();
+
+    let device_name = "pulse";
+    let device = host
+        .output_devices()
+        .unwrap()
+        .find(|d| d.name().unwrap() == device_name)
+        .expect("Failed to find specified output device");
+
+    let mut supported_configs = device.supported_output_configs().unwrap();
+
+    let config: StreamConfig = supported_configs.next().into();
+
+    let err_fn = |err| eprintln!("an error occurred on the output audio stream: {}", err);
+
+    let sample = Arc::new(Mutex::new(0.0f32));
+
+    let write_tone = move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+        let mut sample = sample.lock().unwrap();
+        let frequency = 80.0;
+        let sample_rate = 44100.0;
+        let amplitude = 0.2;
+        for i in 0..data.len() {
+            let phase = (i as f32 / sample_rate) * frequency * 2.0 * std::f32::consts::PI;
+            *sample = amplitude * phase.sin();
+            data[i] = *sample;
+        }
+    };
+
+    let stream = device
+        .build_output_stream(&config, write_tone, err_fn, None)
+        .unwrap();
+
+    stream.play().unwrap();
+
+    std::thread::park();
 }
